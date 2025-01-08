cb9f72f47040f906d0f2b376de9166d0064bdbec
AbdulrhmanAhmed <Abdalrhman999109hotmail.comgit config --global user.email Abdalrhman999109hotmail.com>
2025-01-08 12:57:23 +0300

    event and hook

--- freact-blog/src/Home.js
+++ freact-blog/src/Home.js
@@ -1,7 +1,30 @@
+import { useState } from "react"
+
 const Home = () => {
+    const handleClick=() => {
+        console.log('hello , programmers')
+    }
+    /*const handleClickag=(name) => {
+        console.log('hello' + name)
+    }*/
+    const [name , setname] = useState('mario');
+    const [age , setage] = useState(15);
+    const handlechange=() => {
+        setname('Khaled')
+        setage(40)
+    }
+    //let name = 'Ahmed';
+    /*const handlechange=() => {
+        name='Abdulrhman'
+        console.log(name)
+    }*/
+    
     return ( 
         <div className="home">
             <h2>HomePage</h2>
+            <button onClick={handleClick}>click me</button>
+            <p>{name} is {age} years old</p>
+            <button onClick={handlechange}>click me</button> 
         </div>
      );
 }
